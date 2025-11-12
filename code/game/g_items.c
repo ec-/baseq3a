ec-/baseq3a

commit 2c65e577e393a8151c74a84130300ff13a9617cc

Always play warmup sounds and display "FIGHT!" message on client side (except for initial singleplayer spawn) Cleanup warmup code on client side Rework warmup code on server side:  - spawn all items/powerups during warmup  - remove dead bodies, dropped items and all launched projectiles on warmup end  - do not issue global map_restart on warmup end but respawn active players individually  - slightly delay items spawns on warmup end

code/game/g_items.c
@@ -15,16 +15,71 @@
   movers and respawn apropriately.
 */
 
+// initial spawn times after warmup
+// in vq3 most of the items appears in one frame but we will delay that a bit
+// to reduce peak bandwidth and get some nice transition effects
+#define	SPAWN_WEAPONS		333
+#define	SPAWN_ARMOR			1200
+#define	SPAWN_HEALTH		900
+#define	SPAWN_AMMO			600
+#define	SPAWN_HOLDABLE		2500
+#define	SPAWN_MEGAHEALTH	10000
+#define	SPAWN_POWERUP		45000
 
-#define	RESPAWN_ARMOR		25
-#define	RESPAWN_HEALTH		35
-#define	RESPAWN_AMMO		40
-#define	RESPAWN_HOLDABLE	60
-#define	RESPAWN_MEGAHEALTH	35//120
-#define	RESPAWN_POWERUP		120
-
+// periodic respawn times
+// g_weaponRespawn.integer || g_weaponTeamRespawn.integer
+#define	RESPAWN_ARMOR		25000
+#define	RESPAWN_HEALTH		35000
+#define	RESPAWN_AMMO		40000
+#define	RESPAWN_HOLDABLE	60000
+#define	RESPAWN_MEGAHEALTH	35000 //120000
+#define	RESPAWN_POWERUP		120000
 
 //======================================================================
+
+int SpawnTime( gentity_t *ent, qboolean firstSpawn ) 
+{
+	if ( !ent->item )
+		return 0;
+
+	switch( ent->item->giType ) {
+
+	case IT_WEAPON:
+		if ( firstSpawn )
+			return SPAWN_WEAPONS;
+		if ( g_gametype.integer == GT_TEAM )
+			return g_weaponRespawn.value * 1000 ;
+		else
+			return g_weaponTeamRespawn.value * 1000;
+
+	case IT_AMMO:
+		return firstSpawn ? SPAWN_AMMO : RESPAWN_AMMO;
+
+	case IT_ARMOR:
+		return firstSpawn ? SPAWN_ARMOR : RESPAWN_ARMOR;
+
+	case IT_HEALTH:
+		if ( ent->item->quantity == 100 ) // mega health respawns slow
+			return firstSpawn ? SPAWN_MEGAHEALTH : RESPAWN_MEGAHEALTH;
+		else
+			return firstSpawn ? SPAWN_HEALTH : RESPAWN_HEALTH;
+
+	case IT_POWERUP:
+		return firstSpawn ? SPAWN_POWERUP : RESPAWN_POWERUP;
+
+#ifdef MISSIONPACK
+	case IT_PERSISTANT_POWERUP:
+		return -1;
+		break;
+#endif
+
+	case IT_HOLDABLE:
+		return firstSpawn ? SPAWN_HOLDABLE : RESPAWN_HOLDABLE;
+	}
+
+	return 0;
+} 
+
 
 int Pickup_Powerup( gentity_t *ent, gentity_t *other ) {
 	int			quantity;
@@ -32,10 +87,8 @@ int Pickup_Powerup( gentity_t *ent, gentity_t *other ) {
 	gclient_t	*client;
 
 	if ( !other->client->ps.powerups[ent->item->giTag] ) {
-		// round timing to seconds to make multiple powerup timers
-		// count in sync
-		other->client->ps.powerups[ent->item->giTag] = 
-			level.time - ( level.time % 1000 );
+		// round timing to seconds to make multiple powerup timers count in sync
+		other->client->ps.powerups[ent->item->giTag] = level.time - ( level.time % 1000 );
 	}
 
 	if ( ent->count ) {
@@ -57,18 +110,18 @@ int Pickup_Powerup( gentity_t *ent, gentity_t *other ) {
 		if ( client == other->client ) {
 			continue;
 		}
-		if ( client->pers.connected == CON_DISCONNECTED ) {
+		if ( client->pers.connected != CON_CONNECTED ) {
 			continue;
 		}
 		if ( client->ps.stats[STAT_HEALTH] <= 0 ) {
 			continue;
 		}
 
-    // if same team in team game, no sound
-    // cannot use OnSameTeam as it expects to g_entities, not clients
-  	if ( g_gametype.integer >= GT_TEAM && other->client->sess.sessionTeam == client->sess.sessionTeam  ) {
-      continue;
-    }
+		// if same team in team game, no sound
+		// cannot use OnSameTeam as it expects to g_entities, not clients
+		if ( g_gametype.integer >= GT_TEAM && other->client->sess.sessionTeam == client->sess.sessionTeam  ) {
+			continue;
+		}
 
 		// if too far away, no sound
 		VectorSubtract( ent->s.pos.trBase, client->ps.origin, delta );
@@ -92,8 +145,10 @@ int Pickup_Powerup( gentity_t *ent, gentity_t *other ) {
 		// anti-reward
 		client->ps.persistant[PERS_PLAYEREVENTS] ^= PLAYEREVENT_DENIEDREWARD;
 	}
-	return RESPAWN_POWERUP;
+
+	return SpawnTime( ent, qfalse ); // return RESPAWN_POWERUP;
 }
+
 
 //======================================================================
 
@@ -166,7 +221,7 @@ int Pickup_PersistantPowerup( gentity_t *ent, gentity_t *other ) {
 		break;
 	}
 
-	return -1;
+	return SpawnTime( ent, qfalse ); // return -1;
 }
 
 //======================================================================
@@ -182,14 +237,14 @@ int Pickup_Holdable( gentity_t *ent, gentity_t *other ) {
 	}
 #endif
 
-	return RESPAWN_HOLDABLE;
+	return SpawnTime( ent, qfalse ); // return RESPAWN_HOLDABLE;
 }
 
 
 //======================================================================
 
 
-void Add_Ammo (gentity_t *ent, int weapon, int count)
+static void Add_Ammo( gentity_t *ent, int weapon, int count )
 {
 	ent->client->ps.ammo[weapon] += count;
 	if ( ent->client->ps.ammo[weapon] > AMMO_HARD_LIMIT ) {
@@ -198,7 +253,7 @@ void Add_Ammo (gentity_t *ent, int weapon, int count)
 }
 
 
-int Pickup_Ammo (gentity_t *ent, gentity_t *other)
+static int Pickup_Ammo( gentity_t *ent, gentity_t *other )
 {
 	int		quantity;
 
@@ -208,15 +263,15 @@ int Pickup_Ammo (gentity_t *ent, gentity_t *other)
 		quantity = ent->item->quantity;
 	}
 
-	Add_Ammo (other, ent->item->giTag, quantity);
+	Add_Ammo( other, ent->item->giTag, quantity );
 
-	return RESPAWN_AMMO;
+	return SpawnTime( ent, qfalse ); // return RESPAWN_AMMO;
 }
 
 //======================================================================
 
 
-int Pickup_Weapon (gentity_t *ent, gentity_t *other) {
+static int Pickup_Weapon( gentity_t *ent, gentity_t *other ) {
 	int		quantity;
 
 	if ( ent->count < 0 ) {
@@ -249,17 +304,18 @@ int Pickup_Weapon (gentity_t *ent, gentity_t *other) {
 		other->client->ps.ammo[ent->item->giTag] = -1; // unlimited ammo
 
 	// team deathmatch has slow weapon respawns
-	if ( g_gametype.integer == GT_TEAM ) {
-		return g_weaponTeamRespawn.integer;
-	}
-
-	return g_weaponRespawn.integer;
+	//if ( g_gametype.integer == GT_TEAM ) {
+	//	return g_weaponTeamRespawn.integer;
+	//} else {
+	//	return g_weaponRespawn.integer;
+	//}
+	return SpawnTime( ent, qfalse );
 }
 
 
 //======================================================================
 
-int Pickup_Health (gentity_t *ent, gentity_t *other) {
+static int Pickup_Health( gentity_t *ent, gentity_t *other ) {
 	int			max;
 	int			quantity;
 
@@ -289,12 +345,14 @@ int Pickup_Health (gentity_t *ent, gentity_t *other) {
 	}
 	other->client->ps.stats[STAT_HEALTH] = other->health;
 
-	if ( ent->item->quantity == 100 ) {		// mega health respawns slow
-		return RESPAWN_MEGAHEALTH;
-	}
-
-	return RESPAWN_HEALTH;
+	//if ( ent->item->quantity == 100 ) { // mega health respawns slow
+	//	return RESPAWN_MEGAHEALTH;
+	//} else {
+	//	return RESPAWN_HEALTH;
+	//}
+	return SpawnTime( ent, qfalse );
 }
+
 
 //======================================================================
 
@@ -321,7 +379,7 @@ int Pickup_Armor( gentity_t *ent, gentity_t *other ) {
 	}
 #endif
 
-	return RESPAWN_ARMOR;
+	return SpawnTime( ent, qfalse ); // return RESPAWN_ARMOR;
 }
 
 //======================================================================
@@ -356,8 +414,9 @@ void RespawnItem( gentity_t *ent ) {
 			;
 	}
 
-	if (!ent)
+	if ( !ent )
 		return;
+
 	ent->r.contents = CONTENTS_TRIGGER;
 	ent->s.eFlags &= ~EF_NODRAW;
 	ent->r.svFlags &= ~SVF_NOCLIENT;
@@ -429,11 +488,9 @@ void Touch_Item (gentity_t *ent, gentity_t *other, trace_t *trace) {
 	switch( ent->item->giType ) {
 	case IT_WEAPON:
 		respawn = Pickup_Weapon(ent, other);
-//		predict = qfalse;
 		break;
 	case IT_AMMO:
 		respawn = Pickup_Ammo(ent, other);
-//		predict = qfalse;
 		break;
 	case IT_ARMOR:
 		respawn = Pickup_Armor(ent, other);
@@ -469,7 +526,7 @@ void Touch_Item (gentity_t *ent, gentity_t *other, trace_t *trace) {
 	}
 
 	// play the normal pickup sound
-	if (predict) {
+	if ( predict ) {
 		G_AddPredictableEvent( other, EV_ITEM_PICKUP, ent->s.modelindex );
 	} else {
 		G_AddEvent( other, EV_ITEM_PICKUP, ent->s.modelindex );
@@ -510,13 +567,14 @@ void Touch_Item (gentity_t *ent, gentity_t *other, trace_t *trace) {
 	// non zero wait overrides respawn time
 	if ( ent->wait ) {
 		respawn = ent->wait;
+		respawn *= 1000;
 	}
 
 	// random can be used to vary the respawn time
 	if ( ent->random ) {
-		respawn += crandom() * ent->random;
-		if ( respawn < 1 ) {
-			respawn = 1;
+		respawn += (crandom() * ent->random) * 1000;
+		if ( respawn < 1000 ) {
+			respawn = 1000;
 		}
 	}
 
@@ -540,9 +598,10 @@ void Touch_Item (gentity_t *ent, gentity_t *other, trace_t *trace) {
 		ent->nextthink = 0;
 		ent->think = 0;
 	} else {
-		ent->nextthink = level.time + respawn * 1000;
+		ent->nextthink = level.time + respawn;
 		ent->think = RespawnItem;
 	}
+
 	trap_LinkEntity( ent );
 }
 
@@ -692,18 +751,6 @@ void FinishSpawningItem( gentity_t *ent ) {
 	if ( ( ent->flags & FL_TEAMSLAVE ) || ent->targetname ) {
 		ent->s.eFlags |= EF_NODRAW;
 		ent->r.contents = 0;
-		return;
-	}
-
-	// powerups don't spawn in for a while
-	if ( ent->item->giType == IT_POWERUP ) {
-		float	respawn;
-
-		respawn = 45 + crandom() * 15;
-		ent->s.eFlags |= EF_NODRAW;
-		ent->r.contents = 0;
-		ent->nextthink = level.time + respawn * 1000;
-		ent->think = RespawnItem;
 		return;
 	}
 

code/cgame/cg_draw.c
@@ -2374,27 +2374,24 @@ CG_DrawWarmup
 */
 static void CG_DrawWarmup( void ) {
 	int			w;
-	int			sec;
 	int			i;
 	float scale;
-	clientInfo_t	*ci1, *ci2;
+	clientInfo_t *ci1, *ci2;
 	int			cw;
 	const char	*s;
 
-	sec = cg.warmup;
-	if ( !sec ) {
+	if ( !cg.warmup ) {
 		return;
 	}
 
-	if ( sec < 0 ) {
+	if ( cg.warmup < 0 ) {
 		s = "Waiting for players";		
 		w = CG_DrawStrlen( s ) * BIGCHAR_WIDTH;
 		CG_DrawBigString(320 - w / 2, 24, s, 1.0F);
-		cg.warmupCount = 0;
 		return;
 	}
 
-	if (cgs.gametype == GT_TOURNAMENT) {
+	if ( cgs.gametype == GT_TOURNAMENT ) {
 		// find the two active players
 		ci1 = NULL;
 		ci2 = NULL;
@@ -2457,39 +2454,22 @@ static void CG_DrawWarmup( void ) {
 #endif
 	}
 
-	sec = ( sec - cg.time ) / 1000;
-	if ( sec < 0 ) {
-		cg.warmup = 0;
-		sec = 0;
-	}
-	s = va( "Starts in: %i", sec + 1 );
-	if ( sec != cg.warmupCount ) {
-		cg.warmupCount = sec;
-		switch ( sec ) {
-		case 0:
-			trap_S_StartLocalSound( cgs.media.count1Sound, CHAN_ANNOUNCER );
-			break;
-		case 1:
-			trap_S_StartLocalSound( cgs.media.count2Sound, CHAN_ANNOUNCER );
-			break;
-		case 2:
-			trap_S_StartLocalSound( cgs.media.count3Sound, CHAN_ANNOUNCER );
-			break;
-		default:
-			break;
-		}
-	}
+	if ( cg.warmupCount <= 0 )
+		return;
+
+	s = va( "Starts in: %i", cg.warmupCount );
+
 	scale = 0.45f;
 	switch ( cg.warmupCount ) {
-	case 0:
+	case 1:
 		cw = 28;
 		scale = 0.54f;
 		break;
-	case 1:
+	case 2:
 		cw = 24;
 		scale = 0.51f;
 		break;
-	case 2:
+	case 3:
 		cw = 20;
 		scale = 0.48f;
 		break;
@@ -2500,12 +2480,11 @@ static void CG_DrawWarmup( void ) {
 	}
 
 #ifdef MISSIONPACK
-		w = CG_Text_Width(s, scale, 0);
-		CG_Text_Paint(320 - w / 2, 125, scale, colorWhite, s, 0, 0, ITEM_TEXTSTYLE_SHADOWEDMORE);
+	w = CG_Text_Width(s, scale, 0);
+	CG_Text_Paint(320 - w / 2, 125, scale, colorWhite, s, 0, 0, ITEM_TEXTSTYLE_SHADOWEDMORE);
 #else
 	w = CG_DrawStrlen( s );
-	CG_DrawStringExt( 320 - w * cw/2, 70, s, colorWhite, 
-			qfalse, qtrue, cw, (int)(cw * 1.5), 0 );
+	CG_DrawStringExt( 320 - w * cw/2, 70, s, colorWhite, qfalse, qtrue, cw, (int)(cw * 1.5), 0 );
 #endif
 }
 
@@ -2535,7 +2514,7 @@ void CG_DrawTimedMenus( void ) {
 CG_Draw2D
 =================
 */
-static void CG_Draw2D(stereoFrame_t stereoFrame)
+static void CG_Draw2D( stereoFrame_t stereoFrame )
 {
 #ifdef MISSIONPACK
 	if (cgs.orderPending && cg.time > cgs.orderTime) {
@@ -2659,6 +2638,60 @@ static void CG_CalculatePing( void ) {
 		cg.meanPing /= count;
 	}
 }
+
+
+static void CG_WarmupEvents( void ) {
+
+	int	count;
+
+	if ( !cg.warmup )
+		return;
+
+	if ( cg.warmup < 0 ) {
+		cg.warmupCount = -1;
+		return;
+	}
+
+	if ( cg.warmup < cg.time ) {
+		cg.warmup = 0;
+		count = 0;
+	} else {
+		count = ( cg.warmup - cg.time + 999 ) / 1000;
+	}
+
+	if ( cg.warmupCount == count ) {
+		return;
+	}
+
+	cg.warmupCount = count;
+	cg.timelimitWarnings = 0;
+
+	switch ( count ) {
+		case 0:
+			if ( cg.warmupFightSound <= cg.time ) {
+				trap_S_StartLocalSound( cgs.media.countFightSound, CHAN_ANNOUNCER );
+				cg.warmupFightSound = cg.time + 750;
+			}
+			CG_CenterPrint( "FIGHT!", 120, GIANTCHAR_WIDTH*2 );
+			break;
+
+		case 1:
+			trap_S_StartLocalSound( cgs.media.count1Sound, CHAN_ANNOUNCER );
+			break;
+
+		case 2:
+			trap_S_StartLocalSound( cgs.media.count2Sound, CHAN_ANNOUNCER );
+			break;
+
+		case 3:
+			trap_S_StartLocalSound( cgs.media.count3Sound, CHAN_ANNOUNCER );
+			break;
+
+		default:
+			break;
+	}
+}
+
 
 
 // will be called on warmup end and when client changed
@@ -2795,6 +2828,9 @@ void CG_DrawActive( stereoFrame_t stereoView ) {
 
 	// draw 3D view
 	trap_R_RenderScene( &cg.refdef );
+
+	// play warmup sounds and display text
+	CG_WarmupEvents();
 
 	// draw status bar and other floating elements
  	CG_Draw2D( stereoView );

code/cgame/cg_local.h
@@ -613,9 +613,11 @@ typedef struct {
 	int			voiceChatBufferIn;
 	int			voiceChatBufferOut;
 #endif
+
 	// warmup countdown
 	int			warmup;
 	int			warmupCount;
+	int			warmupFightSound;	// last time "Fight!" sound was played
 
 	//==========================
 

code/cgame/cg_playerstate.c
@@ -432,7 +432,7 @@ void CG_CheckLocalSounds( playerState_t *ps, playerState_t *ops ) {
 	}
 
 	// timelimit warnings
-	if ( cgs.timelimit > 0 && !cg.warmup ) {
+	if ( cgs.timelimit > 0 && !cg.warmup && cg.warmupFightSound < cg.time ) {
 		int		msec;
 
 		msec = cg.time - cgs.levelStartTime;

code/cgame/cg_servercmds.c
@@ -168,6 +168,20 @@ static void CG_ParseWarmup( void ) {
 	warmup = atoi( info );
 	cg.warmupCount = -1;
 
+	if ( warmup ) {
+		cg.timelimitWarnings |= 1 | 2 | 4;
+		cg.fraglimitWarnings |= 1 | 2 | 4;
+	}
+
+	if ( cg.clientFrame == 0 ) {
+		if ( warmup == 0 && cgs.gametype != GT_SINGLE_PLAYER ) {
+			// force sound playback in CG_WarmupEvents()
+			cg.warmup = cg.time;
+			cg.warmupCount = -1;
+			return;
+		}
+	}
+
 	if ( warmup == 0 && cg.warmup ) {
 
 	} else if ( warmup > 0 && cg.warmup <= 0 ) {
@@ -443,8 +457,8 @@ static void CG_MapRestart( void ) {
 
 	// make sure the "3 frags left" warnings play again
 	cg.fraglimitWarnings = 0;
-
 	cg.timelimitWarnings = 0;
+
 	cg.rewardTime = 0;
 	cg.rewardStack = 0;
 	cg.intermissionStarted = qfalse;
@@ -456,15 +470,19 @@ static void CG_MapRestart( void ) {
 
 	CG_StartMusic();
 
-	trap_S_ClearLoopingSounds(qtrue);
+	trap_S_ClearLoopingSounds( qtrue );
+
+	cg.allowPickupPrediction = cg.time + PICKUP_PREDICTION_DELAY;
 
 	// we really should clear more parts of cg here and stop sounds
 
 	// play the "fight" sound if this is a restart without warmup
-	if ( cg.warmup == 0 /* && cgs.gametype == GT_TOURNAMENT */) {
-		trap_S_StartLocalSound( cgs.media.countFightSound, CHAN_ANNOUNCER );
-		CG_CenterPrint( "FIGHT!", 120, GIANTCHAR_WIDTH*2 );
+	if ( cg.warmup == 0 /* && cgs.gametype == GT_TOURNAMENT */ ) {
+		// force sound playback in CG_WarmupEvents()
+		cg.warmup = cg.time;
+		cg.warmupCount = -1;
 	}
+
 #ifdef MISSIONPACK
 	if (cg_singlePlayerActive.integer) {
 		trap_Cvar_Set("ui_matchStartTime", va("%i", cg.time));
@@ -473,7 +491,8 @@ static void CG_MapRestart( void ) {
 		}
 	}
 #endif
-	trap_Cvar_Set("cg_thirdPerson", "0");
+
+	trap_Cvar_Set( "cg_thirdPerson", "0" );
 }
 
 #ifdef MISSIONPACK

code/game/g_local.h
@@ -470,6 +470,7 @@ void G_RevertVote( gclient_t *client );
 void G_CheckTeamItems( void );
 void G_RunItem( gentity_t *ent );
 void RespawnItem( gentity_t *ent );
+int SpawnTime( gentity_t *ent, qboolean firstSpawn );
 
 void UseHoldableItem( gentity_t *ent );
 void PrecacheItem (gitem_t *it);
@@ -480,7 +481,6 @@ void G_SpawnItem (gentity_t *ent, gitem_t *item);
 void FinishSpawningItem( gentity_t *ent );
 void Think_Weapon (gentity_t *ent);
 int ArmorIndex (gentity_t *ent);
-void	Add_Ammo (gentity_t *ent, int weapon, int count);
 void Touch_Item (gentity_t *ent, gentity_t *other, trace_t *trace);
 
 void ClearRegisteredItems( void );
@@ -668,6 +668,7 @@ void G_RunClient( gentity_t *ent );
 qboolean OnSameTeam( gentity_t *ent1, gentity_t *ent2 );
 void Team_CheckDroppedItem( gentity_t *dropped );
 qboolean CheckObeliskAttack( gentity_t *obelisk, gentity_t *attacker );
+void Team_ResetFlags( void );
 
 //
 // g_mem.c

code/game/g_main.c
@@ -1517,6 +1517,116 @@ static void CheckExitRules( void ) {
 }
 
 
+static void ClearBodyQue( void ) {
+	int	i;
+	gentity_t	*ent;
+
+	for ( i = 0 ; i < BODY_QUEUE_SIZE ; i++ ) {
+		ent = level.bodyQue[ i ];
+		if ( ent->r.linked || ent->physicsObject ) {
+			trap_UnlinkEntity( ent );
+			ent->physicsObject = qfalse;
+		}
+	}
+}
+
+
+static void G_WarmupEnd( void ) 
+{
+	gclient_t *client;
+	gentity_t *ent;
+	int i, t;
+
+	// remove corpses
+	ClearBodyQue();
+
+	// return flags
+	Team_ResetFlags();
+
+	memset( level.teamScores, 0, sizeof( level.teamScores ) );
+
+	level.warmupTime = 0;
+	level.startTime = level.time;
+
+	trap_SetConfigstring( CS_SCORES1, "0" );
+	trap_SetConfigstring( CS_SCORES2, "0" );
+	trap_SetConfigstring( CS_WARMUP, "" );
+	trap_SetConfigstring( CS_LEVEL_START_TIME, va( "%i", level.startTime ) );
+	
+	client = level.clients;
+	for ( i = 0; i < level.maxclients; i++, client++ ) {
+		
+		if ( client->pers.connected != CON_CONNECTED )
+			continue;
+
+		// reset player awards
+		client->ps.persistant[PERS_IMPRESSIVE_COUNT] = 0;
+		client->ps.persistant[PERS_EXCELLENT_COUNT] = 0;
+		client->ps.persistant[PERS_DEFEND_COUNT] = 0;
+		client->ps.persistant[PERS_ASSIST_COUNT] = 0;
+		client->ps.persistant[PERS_GAUNTLET_FRAG_COUNT] = 0;
+
+		client->ps.persistant[PERS_SCORE] = 0;
+		client->ps.persistant[PERS_CAPTURES] = 0;
+
+		client->ps.persistant[PERS_ATTACKER] = ENTITYNUM_NONE;
+		client->ps.persistant[PERS_ATTACKEE_ARMOR] = 0;
+		client->damage.enemy = client->damage.team = 0;	
+
+		client->ps.stats[STAT_CLIENTS_READY] = 0;
+		client->ps.stats[STAT_HOLDABLE_ITEM] = 0;
+
+		memset( &client->ps.powerups, 0, sizeof( client->ps.powerups ) );
+
+		ClientUserinfoChanged( i ); // set max.health etc.
+
+		if ( client->sess.sessionTeam != TEAM_SPECTATOR ) {
+			ClientSpawn( level.gentities + i );
+		}
+
+		trap_SendServerCommand( i, "map_restart" );
+	}
+
+	// respawn items, remove projectiles, etc.
+	ent = level.gentities + MAX_CLIENTS;
+	for ( i = MAX_CLIENTS; i < level.num_entities ; i++, ent++ ) {
+
+		if ( !ent->inuse || ent->freeAfterEvent )
+			continue;
+
+		if ( ent->s.eType == ET_ITEM && ent->item ) {
+			
+			// remove dropped items
+			if ( ent->flags & FL_DROPPED_ITEM ) {
+				ent->nextthink = level.time;
+				continue;
+			}
+
+			// respawn picked up items
+			t = SpawnTime( ent, qtrue );
+			if ( t != 0 ) {
+				// hide items with defined spawn time
+				ent->s.eFlags |= EF_NODRAW;
+				ent->r.svFlags |= SVF_NOCLIENT;
+				ent->r.contents = 0;
+				ent->activator = NULL;
+				ent->think = RespawnItem;
+				ent->nextthink = level.time + t;
+			} else {
+				if ( ent->activator ) {
+					ent->activator = NULL;
+					ent->think = RespawnItem;
+				}
+				ent->nextthink = level.time + FRAMETIME;
+			}
+
+		} else if ( ent->s.eType == ET_MISSILE ) {
+			// remove all launched missiles
+			G_FreeEntity( ent );
+		}
+	}
+}
+
 
 /*
 ========================================================================
@@ -1585,10 +1695,7 @@ void CheckTournament( void ) {
 
 		// if the warmup time has counted down, restart
 		if ( level.time > level.warmupTime ) {
-			level.warmupTime = level.time + 10000;
-			trap_Cvar_Set( "g_restarted", "1" );
-			trap_SendConsoleCommand( EXEC_APPEND, "map_restart 0\n" );
-			level.restarted = qtrue;
+			G_WarmupEnd();
 			return;
 		}
 	} else if ( g_gametype.integer != GT_SINGLE_PLAYER && level.warmupTime != 0 ) {
@@ -1639,10 +1746,7 @@ void CheckTournament( void ) {
 
 		// if the warmup time has counted down, restart
 		if ( level.time > level.warmupTime ) {
-			level.warmupTime = level.time + 10000;
-			trap_Cvar_Set( "g_restarted", "1" );
-			trap_SendConsoleCommand( EXEC_APPEND, "map_restart 0\n" );
-			level.restarted = qtrue;
+			G_WarmupEnd();
 			return;
 		}
 	}

code/game/g_spawn.c
@@ -592,7 +592,11 @@ void SP_worldspawn( void ) {
 	} else {
 		// assume that g_doWarmup is always 1
 		level.warmupTime = -1;
-		trap_SetConfigstring( CS_WARMUP, va( "%i", level.warmupTime ) );
+		if ( g_warmup.integer > 0 ) {
+			trap_SetConfigstring( CS_WARMUP, va( "%i", level.warmupTime ) );
+		} else {
+			trap_SetConfigstring( CS_WARMUP, "" );
+		}
 		G_LogPrintf( "Warmup:\n" );
 	}
 }

commit 7de0e0f651a879321a82ee4ecb68492aeead95e7

fix: don't spawn powerups immediately
Closes https://github.com/ec-/baseq3a/issues/8.

The bug has been introduced in
2c65e577e393a8151c74a84130300ff13a9617cc.
This commit basically brings back the code that was removed
as part of that one.

The intent of that commit was to spawn the powerups immediately
for warmup, but then spawn them as usual when the warmup ends,
in `G_WarmupEnd`.
However, it's not always that we get to execute `G_WarmupEnd`.
Namely, it doesn't get executed when `g_warmup 0`
or in single player matches.

The downside is, of course, that we no longer spawn the powerups
for warmup immediately.

So let's simply revert that change until a better solution is found.

code/game/g_items.c
@@ -758,6 +758,18 @@ void FinishSpawningItem( gentity_t *ent ) {
 		return;
 	}
 
+	// powerups don't spawn in for a while
+	if ( ent->item->giType == IT_POWERUP ) {
+		int	respawn;
+
+		respawn = SPAWN_POWERUP + crandom() * 15 * 1000;
+		ent->s.eFlags |= EF_NODRAW;
+		ent->r.contents = 0;
+		ent->nextthink = level.time + respawn;
+		ent->think = RespawnItem;
+		return;
+	}
+
 	trap_LinkEntity( ent );
 }
 